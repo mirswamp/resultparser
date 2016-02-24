2baf4b7f6f4a0389ca9b9bd08fadd67277a655ed
Ashish Vishwanath Shenoy <ashenoy@balthazar.cs.wisc.edu>
2016-02-24 14:44:24 -0600

    Adding the gccwarn parser

---
+++ scripts/gccwarn.pl
@@ -0,0 +1,218 @@
+#!/usr/bin/perl -w
+
+#use strict;
+use Getopt::Long;
+use bugInstance;
+use XML::Twig;
+use xmlWriterObject;
+use util;
+
+my (
+	$input_file,   $output_file, $tool_name, $tool_version, $uuid,
+	$package_name, $build_id,    $cwd,       $replace_dir,  $file_path
+);
+my $violationId = 0;
+my $bugId       = 0;
+my $locationId  = 0;
+my $file_Id     = 0;
+
+my $prev_line = "";
+my $trace_start_line = 1;
+my $current_line_no;
+my $fn_file;
+my $function;
+my $line;
+my $message;
+
+
+GetOptions(
+	"input_file=s"     => \$input_file,
+	"output_file=s"    => \$output_file,
+	"tool_name=s"      => \$tool_name,
+	"tool_version=s"   => \$tool_version,
+	"package_name=s"   => \$package_name,
+	"uuid=s"           => \$uuid,
+	"build_id=s"       => \$build_id,
+	"cwd=s"            => \$cwd,
+	"replace_dir=s"    => \$replace_dir,
+	"input_file_arr=s" => \@input_file_arr
+) or die("Error");
+
+my $xmlWriterObj = new xmlWriterObject($output_file);
+$xmlWriterObj->addStartTag( $tool_name, $tool_version, $uuid );
+my $bugObject;
+
+foreach my $input_file (@input_file_arr) {
+	$prev_msg="";
+	$prev_bug_group="";
+    $prev_fn="";
+    $prev_line="";
+    $trace_start_line=1;
+    $locationId=0;
+    $methodId=0;
+    $function = "";
+    $fn_file = "";
+    
+	my $input = new IO::File("<$input_file");
+    LINE:
+	while ( my $line = <$input> ) {
+		print "Processing".$input_file."\n";
+		chomp($line);
+		$current_line_no = $.;
+		if ( $line eq $prev_line ) {
+			next LINE;
+		}
+		else {
+			$prev_line = $line;
+		}
+		my $valid = &validate_line($line);
+		#print "*****".$valid."****";
+		if ( $valid eq "function" ) {
+			my @tokens = util::split_string($line);
+			$fn_file = $tokens[0];
+			$function = $tokens[1];
+			$function =~ /‘(.*)’/;
+			$function = $1;
+		} elsif ( $valid ne "invalid" ) {
+			#print "BEFORE PARSE";
+			&parse_line( $current_line_no, $line, $function, $fn_file );
+			#print "OVER HERE AFTER PARSE";
+			#print "####".$function;
+		}
+	}
+	&register_bugpath($current_line_no);
+}
+$xmlWriterObj->writeSummary();
+$xmlWriterObj->addEndTag();
+
+sub parse_line {
+	my ( $bug_report_line, $line, $function, $fn_line ) = @_;
+	my @tokens = util::split_string($line);
+	my $num_of_tokens = @tokens;
+	my ( $file, $line_no, $col_no, $bug_group, $message );
+	my $flag = 1;
+	#print "NUM OF TOKENS".$num_of_tokens;
+	if ( $num_of_tokens eq 5 ) {
+		$file      = util::AdjustPath( $package_name, $cwd, $tokens[0] );
+		$line_no   = $tokens[1];
+		$col_no    = $tokens[2];
+		$bug_group = $tokens[3];
+		$message   = $tokens[4];
+	}
+	elsif ( $num_of_tokens eq 4 ) {
+		$file      = util::AdjustPath( $package_name, $cwd, $tokens[0] );
+		$line_no   = $tokens[1];
+		$col_no    = 0;
+		$bug_group = $tokens[2];
+		$message   = $tokens[3];
+	}
+	else {
+
+		#bad line. hence skipping.
+		$flag = 0;
+	}
+
+	if ( $flag ne 0 ) {
+		$bug_group = util::trim($bug_group);
+		$message   = util::trim($message);
+		&register_bug( $bug_report_line, $function, $fn_line, $file, $line_no,
+			$col_no, $bug_group, $message );
+	}
+}
+
+sub register_bugpath {
+	my ($bug_report_line) = @_;
+	my ( $bugLineStart, $bugLineEnd );
+	if ( $bugId > 0 ) {    #Store the information for prev bug trace
+		if ( $trace_start_line eq $bug_report_line - 1 ) {
+			$bugLineStart = $trace_start_line;
+			$bugLineEnd   = $trace_start_line;
+		}
+		else {
+			$bugLineStart = $trace_start_line;
+			$bugLineEnd   = $bug_report_line - 1;
+		}
+		$bugObject->setBugLine( $bugLineStart, $bugLineEnd );
+		$trace_start_line = $bug_report_line;
+	}
+}
+
+sub register_bug {
+	my ( $bug_report_line, $function, $fn_file, $file, $line_no, $col_no, $bug_group, $message ) = @_;
+
+	if ( $bug_group eq "note" and $bugId > 0 ) {
+		$bugObject->setBugLocation(++$locationId, "", $file, $line_no, $line_no, $col_no, 0, $message,"false", "true");
+		$prev_msg       = $message;
+		$prev_bug_group = $bug_group;
+		$prev_fn        = $function;
+		$xmlWriterObj->writeBugObject($bugObject);
+		undef $bugObject;
+		return;
+	}
+	if ($fn_file ne $file or $prev_msg ne $message or $prev_bug_group ne $bug_group or $prev_fn ne $function or $locationId > 99 ) {
+		if(defined $bugObject){
+			$xmlWriterObj->writeBugObject($bugObject);
+			undef $bugObject;
+		}
+		$bugObject = new bugInstance($bugId);
+		&register_bugpath($bug_report_line);
+		$bugId++;
+		$methodId   = 0;
+		$locationId = 0;
+		#print "Creating bug object";
+		$bugObject->setBugBuildId($build_id);
+		$bugObject->setBugReportPath($input_file);
+		if ( $function ne '' ) {
+			$bugObject->setBugMethod( ++$methodId, "", $function, "true" );
+		}
+		$bugObject->setBugGroup($bug_group);
+		&parse_message($message);
+	}
+
+	$bugObject->setBugLocation(++$locationId, "", $file, $line_no, $line_no, $col_no, 0, "", "true", "true");
+	$prev_msg       = $message;
+	$prev_bug_group = $bug_group;
+	$prev_fn        = $function;
+}
+
+sub parse_message {
+	my ($message) = @_;
+	my $orig_msg  = $message;
+	my $code      = $message;
+	if ( defined($code) ) {
+		$code =~ /(.*)\[(.*)\]$/;
+		$message = $1;
+		$code    = $2;
+	}
+
+	if ( ( defined $message ) && ( $message ne '' ) ) {
+		$bugObject->setBugMessage($message);
+	}
+	else { $bugObject->setBugMessage($orig_msg) }
+	if ( ( defined $code ) && ( $code ne '' ) ) {
+		$bugObject->setBugCode($code);
+	}
+}
+
+sub validate_line {
+	my ($line) = @_;
+	if ( $line =~ m/^.*: *In .*function.*:$/i ) {
+		return "function";
+	}
+	elsif ( $line =~ m/^.*: *In .*constructor.*:$/i ) {
+		return "function";
+	}
+	elsif ( $line =~ m/.*: *warning *:.*/i ) {
+		return "warning";
+	}
+	elsif ( $line =~ m/.*: *error *:.*/i ) {
+		return "error";
+	}
+	elsif ( $line =~ m/.*: *note *:.*/i ) {
+		return "note";
+	}
+	else {
+		return "invalid";
+	}
+}
+
--- scripts/util.pm
+++ scripts/util.pm
@@ -4,20 +4,18 @@ package util;
 # NormalizePath - take a path and remove empty and '.' directory components
 #                 empty directories become '.'
 #
-sub NormalizePath
-{
-    my $p = shift;
+sub NormalizePath {
+	my $p = shift;
 
-    $p =~ s/\/\/+/\//g;                 # collapse consecutive /'s to one /
-    $p =~ s/\/(\.\/)+/\//g;             # change /./'s to one /
-    $p =~ s/^\.\///;                    # remove initial ./
-    $p = '.' if $p eq '';               # change empty dirs to .
-    $p =~ s/\/\.$/\//;                  # remove trailing . directory names
-    $p =~ s/\/$// unless $p eq '/';     # remove trailing /
+	$p =~ s/\/\/+/\//g;        # collapse consecutive /'s to one /
+	$p =~ s/\/(\.\/)+/\//g;    # change /./'s to one /
+	$p =~ s/^\.\///;           # remove initial ./
+	$p = '.' if $p eq '';      # change empty dirs to .
+	$p =~ s/\/\.$/\//;                 # remove trailing . directory names
+	$p =~ s/\/$// unless $p eq '/';    # remove trailing /
 
-    return $p;
+	return $p;
 }
-
 
 # AdjustPath - take a path that is relative to curDir and make it relative
 #              to baseDir.  If the path is not in baseDir, do not modify.
@@ -26,106 +24,135 @@ sub NormalizePath
 #       curDir     - the directory paths are currently relative to
 #       path       - the path to change
 #
-sub AdjustPath
-{
-    my ($baseDir, $curDir, $path) = @_;
+sub AdjustPath {
+	my ( $baseDir, $curDir, $path ) = @_;
 
-    $baseDir = NormalizePath($baseDir);
-    $curDir = NormalizePath($curDir);
-    $path = NormalizePath($path);
+	$baseDir = NormalizePath($baseDir);
+	$curDir  = NormalizePath($curDir);
+	$path    = NormalizePath($path);
 
-    # if path is relative, prefix with current dir
-    if ($path eq '.')  {
-        $path = $curDir;
-    }  elsif ($curDir ne '.' && $path !~ /^\//)  {
-        $path = "$curDir/$path";
-    }
+	# if path is relative, prefix with current dir
+	if ( $path eq '.' ) {
+		$path = $curDir;
+	}
+	elsif ( $curDir ne '.' && $path !~ /^\// ) {
+		$path = "$curDir/$path";
+	}
 
-    # remove initial baseDir from path if baseDir is not empty
-    $path =~ s/^\Q$baseDir\E\///;
+	# remove initial baseDir from path if baseDir is not empty
+	$path =~ s/^\Q$baseDir\E\///;
 
-    return $path;
+	return $path;
+}
+=pod
+sub parseSummary {
+	my $summary_file = shift;
+	my $twig         = XML::Twig->new();
+	$twig->parsefile($summary_file);
+
+	$uuid_xpath             = '/assessment-summary/assessment-summary-uuid';
+	$package_root_dir_xpath = '/assessment-summary/package-root-dir';
+	$tool_type_xpath        = '/assessment-summary/tool-type';
+	$root_dir_xpath         = '/assessment-summary/assessment-root-dir';
+	$assessment_artifact_xpath =
+	  '/assessment-summary/assessment-artifacts/assessment';
+
+	my $twig = XML::Twig->new(
+		twig_roots => {
+			$uuid_xpath                => 1,
+			$tool_type_xpath           => 1,
+			$tool_version_xpath        => 1,
+			$root_dir_xpath            => 1,
+			$assessment_artifact_xpath => 1,
+		},
+		twig_handlers => {
+			$uuid_xpath             => \&getUuid,
+			$package_root_dir_xpath => \&getPackageName,
+			$tool_type_xpath        => \&getToolName,
+			$tool_version_xpath     => \&getToolVersion,
+			$root_dir_xpath         => \&getAssessmentFiles
+
+		}
+	);
+
+	my @parsed_summary;
+
+	my $root  = $twig->root;
+	my @uuids = $twig->get_xpath('/assessment-summary/assessment-summary-uuid');
+	my $uuid  = $uuids[0]->text;
+
+	my @pkg_dirs     = $twig->get_xpath('/assessment-summary/package-root-dir');
+	my $package_name = $pkg_dirs[0]->text;
+	$package_name =~ s/\/[^\/]*$//;
+
+	my @tool_names = $twig->get_xpath('/assessment-summary/tool-type');
+	my $tool_name  = $tool_names[0]->text;
+
+	my @assessment_root_dir =
+	  $twig->get_xpath('/assessment-summary/assessment-root-dir');
+	my $size = @assessment_root_dir;
+	if ( $size > 0 ) {
+		$package_name = $assessment_root_dir[0]->text;
+	}
+	my @assessments =
+	  $twig->get_xpath('/assessment-summary/assessment-artifacts/assessment');
+
+	foreach my $i (@assessments) {
+		my @report = $i->get_xpath('report');
+		my @target = $i->get_xpath('replace-path/target')
+		  if ( defined $i->get_xpath('replace-path/target') );
+		my @srcdir = $i->get_xpath('replace-path/srcdir')
+		  if ( defined $i->get_xpath('replace-path/srcdir') );
+		my $srcdir_path = " ";
+		if (@srcdir) {
+			$srcdir_path = $target[0]->text;
+			foreach my $elem (@srcdir) {
+				$srcdir_path = $srcdir_path . "::" . $elem->text;
+			}
+		}
+		my @build_art_id      = $i->get_xpath('build-artifact-id');
+		my $build_artifact_id = 0;
+		my @cwd               = $i->get_xpath('command/cwd');
+		$build_artifact_id = $build_art_id[0]->text
+		  if defined( $build_art_id[0] );
+		push(
+			@parsed_summary,
+			join( "~:~",
+				$uuid,         $package_name,      $tool_name,
+				$tool_version, $build_artifact_id, $report[0]->text,
+				$cwd[0]->text, $srcdir_path )
+		) if defined( $report[0] );
+	}
+
+	return @parsed_summary;
+}
+=cut
+sub split_string {
+	my ($str) = @_;
+	$str =~ s/::+/~#~/g;
+	$str =~ /(‘[^:]+:+[^:]+’)/;
+	my $temp = $1;
+	$str =~ s/‘[^:]+:+[^:]+’/~~&&~~/;
+	if ( defined($temp) ) {
+		$temp =~ s/:/~%%~/;
+	}
+	$str =~ s/~~&&~~/$temp/;
+	my @tokens = split( ':', $str );
+	my @ret;
+	foreach $a (@tokens) {
+		$a =~ s/~#~/::/g;
+		$a =~ s/~%%~/:/g;
+		push( @ret, $a );
+	}
+	return (@ret);
 }
 
-sub parseSummary
+sub trim
 {
-        my $summary_file = shift;
-        my $twig = XML::Twig->new();
-        $twig->parsefile($summary_file);
-        
-        $uuid_xpath = '/assessment-summary/assessment-summary-uuid';
-        $package_root_dir_xpath = '/assessment-summary/package-root-dir';
-        $tool_type_xpath = '/assessment-summary/tool-type';
-        $tool_version_xpath = '/assessment-summary/tool-version';
-        $root_dir_xpath = '/assessment-summary/assessment-root-dir';
-        $assessment_artifact_xpath = '/assessment-summary/assessment-artifacts/assessment';
-        
-        my $twig = XML::Twig->new(
-                twig_roots=>{$uuid_xpath =>1,
-                	         $tool_type_xpath=>1,
-                	         $tool_version_xpath=>1,
-                	         $root_dir_xpath=>1,
-                	         $assessment_artifact_xpath=>1,
-                	         $tool_version_xpath=>1,
-                },
-                twig_handlers=>{
-                    $uuid_xpath=>\&getUuid,
-                    $package_root_dir_xpath=>\&getPackageName,
-                    $tool_type_xpath=>\&getToolName,
-                    $tool_version_xpath=>\&getToolVersion,
-                    $root_dir_xpath=>\&getAssessmentFiles
-                    
-                });
-        
-        
-        my @parsed_summary;
-
-        my $root=$twig->root;
-        my @uuids = $twig->get_xpath('/assessment-summary/assessment-summary-uuid');
-        my $uuid = $uuids[0]->text;
-        
-        my @pkg_dirs = $twig->get_xpath('/assessment-summary/package-root-dir');
-        my $package_name = $pkg_dirs[0]->text;
-        $package_name =~ s/\/[^\/]*$//;
-
-        my @tool_names = $twig->get_xpath('/assessment-summary/tool-type');
-        my $tool_name = $tool_names[0]->text;
-
-        my @tool_versions = $twig->get_xpath('/assessment-summary/tool-version');
-        my $tool_version = $tool_versions[0]->text;
-        $tool_version =~ s/\n/ /g;
-
-        my @assessment_root_dir =  $twig->get_xpath('/assessment-summary/assessment-root-dir');
-        my $size =  @assessment_root_dir;
-        if ($size > 0)
-        {
-            $package_name = $assessment_root_dir[0]->text;
-        } 
-        my @assessments = $twig->get_xpath('/assessment-summary/assessment-artifacts/assessment');
-        
-
-        foreach my $i (@assessments)
-        {
-            my @report=$i->get_xpath('report');
-            my @target = $i->get_xpath('replace-path/target') if (defined $i->get_xpath('replace-path/target'));
-            my @srcdir = $i->get_xpath('replace-path/srcdir') if (defined $i->get_xpath('replace-path/srcdir'));
-            my $srcdir_path = " ";
-            if (@srcdir)
-            {
-                $srcdir_path = $target[0]->text;    
-                foreach my $elem (@srcdir)
-                {
-                    $srcdir_path = $srcdir_path."::".$elem->text;
-                }
-            }
-            my @build_art_id = $i->get_xpath('build-artifact-id');
-            my $build_artifact_id = 0;
-            my @cwd=$i->get_xpath('command/cwd');   
-            $build_artifact_id = $build_art_id[0]->text if defined ($build_art_id[0]);
-            push(@parsed_summary, join("~:~",$uuid,$package_name,$tool_name,$tool_version,$build_artifact_id,$report[0]->text,$cwd[0]->text,$srcdir_path)) if defined($report[0]);
-        }
-        
-        return @parsed_summary;
+        my ($string) = @_;
+        $string =~ s/^ *//;
+        $string =~ s/ *$//;
+        return "$string";
 }
 
 1;
