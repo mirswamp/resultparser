07b9b3b9919dc2a654d7a8dd8bf6b4e1326c4462
James A. Kupsch <kupsch@cs.wisc.edu>
2019-08-22 13:11:47 -0500

    more improvements to synthesizing the BugCode

--- scripts/flow.pl
+++ scripts/flow.pl
@@ -19,6 +19,43 @@ sub ParseFile
 }
 
 
+# make error message generic instead of including names of objects
+sub DescToBugCode
+{
+    my ($s) = (@_);
+
+    $s =~ s/(^|\s*)(\`(['"]).*?\3\`|\`.*?\`)/$1?/sg;
+    $s =~ s/\s*Did you mean.*\?$//s;
+    $s =~ s/\s+\[\d+\]//g;
+    $s =~ s/(^|\s*)\d+(\s*|$)/$1?$2/g;
+    $s =~ s/(^| )(((empty )?string|number|boolean|(array|object|function) types?|(empty )?array( element)?|object|function)( literal( \?)?)?|global object|null or undefined|null|possibly uninitialized variable|statics|uninitialized variable|empty|new object|undefined|property \?( of unknown type)?|(generator|async) function|(module|class) \?|new Function\(\.{2,}\)|return value|first parameter|second argument|exports|prototype|enum|\? type|(\? )?super( \?)?|type argument)/$1?/g;
+    $s =~ s/\bthe\s+//g;
+    $s =~ s/\?(\s+(in\s+)?\?)+/?/g;
+    $s =~ s/[\s.:]*$//;
+    $s =~ s/(^| )\? is /$1/;
+    $s =~ s/(:\s+Either)(\s+(?:\? is )?(.*?))(?:\.\s+Or\s*(?:\? is )?\3)+$/DescToBugCode($2)/es;
+    $s =~ s/(\s+(in|with|of|using|by)\s+\?)*$//;
+    $s =~ s/^(Cannot call because) no .*argument.*by (\?|Function).*/$1 argument count/;
+    $s =~ s/\ban \?/a ?/g;
+    $s =~ s/ \? (to|and) \?//g;
+    $s =~ s/( \?,?)* or \?/ ?/g;
+    $s =~ s/^\*{3,}\s+|\s+\*{3,}//g;
+    $s =~ s/(\s\?\s+\w+)\s+is/$1s are/g;
+    $s =~ s/\. .*$//;
+    $s =~ s/^(Experimental )\? (usage)\.\s+.*/$1$2/s;
+    $s =~ s/^(Unexpected token)\s+.*/$1/s;
+    $s =~ s/^(Internal error):\s+.*/$1/s;
+    $s =~ s/^(Cannot resolve \w+) \?$/$1/;
+    $s =~ s/^(Cannot call) \?/$1/;
+    $s =~ s/^(Could not decide which case to select)\.\s+.*/$1/s;
+    $s =~ s/^(Missing type annotation) for \?\.\s+.*/$1/s;
+    $s =~ s/^(Unable to determine module type) .*/$1/s;
+    $s =~ s/^\?\s+//;
+
+    return $s;
+}
+
+
 sub WriteFlowWeakness  {
     my ($parser, $e) = @_;
 
@@ -36,7 +73,7 @@ sub WriteFlowWeakness  {
 	++$locCount;
 	my $type = $msg->{type};
 	if (!defined $bugCode || $type eq 'Comment' && !defined  $msg->{context})  {
-	    $bugCode = $msg->{descr};
+	    $bugCode = DescToBugCode($msg->{descr});
 	}
 	$bugMsg .= ' ' if defined $bugMsg;
 	my $locMsg = $msg->{descr};
@@ -56,21 +93,6 @@ sub WriteFlowWeakness  {
 	    $isPrimary = 'false';
 	}
     }
-
-    # make error message generic instead of including names of objects
-    $bugCode =~ s/(^| )(((empty )?string|number|boolean|(empty )?array( element)?|object|function)( literal( `\S*?`)?)?|global object|null or undefined|null|possibly uninitialized variable|statics|uninitialized variable|empty|new object|undefined|property \`\S*?\` of unknown type)/$1?/g;
-    $bugCode =~ s/\`.*?\`/?/g;
-    $bugCode =~ s/\s+\[\d+\]//g;
-    $bugCode =~ s/\s*[.:]$//;
-    $bugCode =~ s/^(Cannot resolve \w+) \?$/$1/;
-    $bugCode =~ s/^\? is //;
-    $bugCode =~ s/(Cannot call \? because): Either \? is (incompatible) with \?(\.\s+Or \? is incompatible with \?)*$/$1 $2/;
-    $bugCode =~ s/(\s+(in|with|of|using)\s+\?)*$//;
-    $bugCode =~ s/^(Cannot call) \?/$1/;
-    $bugCode =~ s/^(Cannot call because) no .*argument.*by (\?|Function).*/$1 argument count/;
-    $bugCode =~ s/ \? is//g;
-    $bugCode =~ s/an \?/a ?/g;
-    $bugCode =~ s/ \? (to|and) \?//g;
 
     $bug->setBugCode($bugCode);
     $bug->setBugMessage($bugMsg);
